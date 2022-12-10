36e87ebf2522a5367cc0e71b9bf34395a9c4ba1d
badcoder2011 <87877801+badcoder2011@users.noreply.github.com>
2022-12-09 23:01:36 -0600

    Completed Day 6

--- Day 6.py
+++ Day 6.py
@@ -1,21 +1,23 @@
 if __name__ == "__main__":
     file = open('input.txt')
     string = file.readline()
-    test = string[0:4]
+    test = string[0:14]
     count, x = 0, 0
     for word in string:
         for let in test:
-            if count == 4:
+            if count == 14:
                 break
             elif test.find(let, test.find(let)+1) == -1:
                 count += 1
             else:
                 count = 0
-        if count == 4:
+        if count == 14:
             break
         else:
             count = 0
         x += 1
-        test = string[x:x + 4]
+        test = string[x:x + 14]
 
-    print(x+4)
+    print(x+14)
+
+    # For part 1 just change all 14's to 4's
